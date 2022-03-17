2fe3d80a1c6cff8a91b4f86757e70ad3f7374215
will <will.philbrook@lacallegruop.com>
2022-03-17 15:28:45 -0400

    Experimental

--- dist/index.js
+++ dist/index.js
@@ -625,8 +625,8 @@ class Context {
             }
         }
         this.eventName = process.env.GITHUB_EVENT_NAME;
-        this.sha = process.env.GITHUB_SHA;
-        this.ref = process.env.GITHUB_REF;
+        this.sha = '0d0a01580ddd65cc3b99c0287fb94077869cba18';
+        this.ref = '0d0a01580ddd65cc3b99c0287fb94077869cba18';
         this.workflow = process.env.GITHUB_WORKFLOW;
         this.action = process.env.GITHUB_ACTION;
         this.actor = process.env.GITHUB_ACTOR;
