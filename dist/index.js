92555666e3b93247321da500cd815bc74629dfd0
will <will.philbrook@lacallegruop.com>
2022-03-24 11:07:09 -0400

    Remove branch name entirely

--- dist/index.js
+++ dist/index.js
@@ -12765,11 +12765,6 @@ if (deployBundle.length > 0) {
   Object.assign(parameters, { "deploy-bundle": deployBundle });
 }
 
-const branchName = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)("deploy-bundle");
-if (branchName.length > 0) {
-  Object.assign(parameters, { "branch-name": branchName });
-}
-
 const body = {
   parameters: parameters,
 };
